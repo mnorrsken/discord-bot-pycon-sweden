24c3ea1fc027ebb26bf9fd12ac0c2126977904cf
mnorrsken <martin.norrsken@gmail.com>
2023-11-09 11:30:47 +0100

    latest

--- bot/cogs/gamer.py
+++ bot/cogs/gamer.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from ranking.engine import get_ranking
 
 class GammerCogs(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -10,3 +10,15 @@ class GammerCogs(commands.Cog):
     async def hello(self, interaction: discord.Interaction) -> None:
         # send a two line message with a banana emoji
         await interaction.response.send_message(f'Hello {interaction.user.name} :banana:!')
+        
+    @discord.app_commands.command(name='rank', description='Bot gets ranking')
+    async def rank(self, interaction: discord.Interaction) -> None:
+        # send a two line message with a banana emoji
+        xp, max_xp, level = get_ranking(interaction.user.id)
+        embed = discord.Embed(title=f'Ranking for {interaction.user.name}', description=f'Level: {level}\nXP: {xp}/{max_xp}', color=discord.Color.blue())
+        embed.add_field(name='XP', value=f'{xp}/{max_xp}')
+        embed.add_field(name='Level', value=level)
+        embed.set_thumbnail(url=interaction.user.display_avatar.url)
+        await interaction.response.send_message(embed=embed)
+
+
